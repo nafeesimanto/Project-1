079c0ed75f2f0b4ca0d01b2fe5fa9d9d30bce97c
Md Nafee Us Nakib Simanto <147148149+nafeesimanto@users.noreply.github.com>
2025-12-14 12:08:37 +0600

    python added

--- main.js
+++ main.js
@@ -17,38 +17,118 @@
 
 // Test 1
 
-function greetUser(name) {
-  console.log(`Hello, ${name}! Welcome back.`);
-}
-greetUser("Nafee");
+// function greetUser(name) {
+//   console.log(`Hello, ${name}! Welcome back.`);
+// }
+// greetUser("Nafee");
 
 // Test 2
 
-function calculateSquare(number) {
-  return number * number;
-}
-console.log(calculateSquare(6));
+// function calculateSquare(number) {
+//   return number * number;
+// }
+// console.log(calculateSquare(6));
 
 // Test 3
 
-const getFullName = (firstName, lastName) => {
-  return `${firstName} ${lastName}`;
-}
-console.log(getFullName("Nafee", "Simanto"));
+// const getFullName = (firstName, lastName) => {
+//   return `${firstName} ${lastName}`;
+// }
+// console.log(getFullName("Nafee", "Simanto"));
 
 // Test 4
 
-function calculateBMI(weight, height) {
-  const bmi = weight / (height * height);
-  return `Your BMI is ${bmi.toFixed(2)}`
+// function calculateBMI(weight, height) {
+//   const bmi = weight / (height * height);
+//   return `Your BMI is ${bmi.toFixed(2)}`
+// }
+// console.log(calculateBMI(70, 1.75));
+
+// Test 5
+
+// {
+//   let x = 5;
+//   var y = 10;
+// }
+// console.log(typeof x);
+// console.log(typeof y);
+
+// Test 6
+
+// console.log(a);
+// var a = 7;
+
+// Test 7
+
+// console.log(b);
+// let b = 15;
+
+// Test 8
+
+// const user = {
+//   name: "Nafee",
+//   show() {
+//     console.log(this.name);
+//   }
+// };
+
+// user.show();
+
+// Test 9
+
+// function testA(a, b) {
+//   console.log(a + b);
+// }
+// testA(10, 20);
+
+// Test 10
+
+// function printName(name) {
+//     return `Hello ${name}`;
+// }
+
+// console.log(printName("Nafee"));
+
+// Test 11
+
+// function outer() {
+//     let count = 0;
+    
+//     function inner() {
+//       count++;
+//       return count;
+//     }
+//     return inner;
+// }
+
+// const counter1 = outer();
+// const counter2 = outer();
+
+// console.log(counter1());
+// console.log(counter2());
+// console.log(counter1());
+
+// array things ↓
+
+// add to start = variableName.unshift(whattoadd)
+// remove from start = variableName.shift()
+// add to end = variableName.push(whattoadd)
+// remove from end = variableName.pop()
+
+
+const names = ["Abdullah", "Rahman", "Fahim", "Kohinur"];
+names.unshift("Nafee");
+names.push("Nafiu");
+
+// // for loop
+
+// for (let i = 0; i < names.length; i++) {
+//   console.log(names[i]);
+  
+// }
+
+// smart loop
+for (let name of names) {
+  console.log(name);
+  
 }
-console.log(calculateBMI(70, 1.75));
-
-
-const person = {
-  name: "Nafee",
-  sayHi() {
-    console.log(this.name);
-  }
-};
-person.sayHi();
